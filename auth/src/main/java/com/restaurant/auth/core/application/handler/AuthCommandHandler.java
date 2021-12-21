c22d6aa2bba1a533d2cec333d15be3eaea78e8e2
ConsolePeasant92 <hugocornel@yahoo.com>
2021-12-21 14:18:26 +0100

    token

--- auth/src/main/java/com/restaurant/auth/core/application/handler/AuthCommandHandler.java
+++ auth/src/main/java/com/restaurant/auth/core/application/handler/AuthCommandHandler.java
@@ -10,34 +10,36 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthCommandHandler {
-    private final UserRepository repository;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
 
-    public AuthCommandHandler(UserRepository repository) {
-        this.repository = repository;
+    public AuthCommandHandler(UserRepository userRepository, TokenRepository tokenRepository) {
+        this.userRepository = userRepository;
+        this.tokenRepository = tokenRepository;
 
         // Create admin account
-        if(this.repository.findById("admin").isEmpty()) {
+        if(this.userRepository.findById("admin").isEmpty()) {
             User admin = new User("admin", "admin", Role.ADMIN, "Ad", "Min", Gender.OTHER);
-            this.repository.save(admin);
+            this.userRepository.save(admin);
         }
     }
 
     public void handle(RegisterUserCommand command) {
-        if(this.repository.findById(command.username()).isPresent()) {
+        if(this.userRepository.findById(command.username()).isPresent()) {
             throw new UserAlreadyExists();
         }
 
         User user = new User(command.username(), command.password(), Role.CUSTOMER, command.firstName(), command.lastName(), Gender.valueOf(command.gender()));
 
-        this.repository.save(user);
+        this.userRepository.save(user);
     }
 
     public String handle(LoginUserCommand command) {
-        if (this.repository.findById(command.username()).isEmpty()) {
+        if (this.userRepository.findById(command.username()).isEmpty()) {
             throw new UserNotFound();
         }
 
-        User user = this.repository.findById(command.username()).get();
+        User user = this.userRepository.findById(command.username()).get();
 
         if (!command.password().equals(user.getPassword())) {
             throw new IncorrectPassword();
@@ -47,13 +49,17 @@ public class AuthCommandHandler {
     }
 
     public void handle(ChangeUserPasswordCommand command) {
-        String username = new StringBuilder(command.token()).reverse().toString();
-
-        if(this.repository.findById(username).isEmpty()) {
+        if(this.tokenRepository.findById(command.token()).isEmpty()) {
             throw new Unauthorized();
         }
 
-        User user = this.repository.findById(username).get();
+        String username = this.tokenRepository.findById(username).get();
+
+        if(this.userRepository.findById(username).isEmpty()) {
+            throw new Unauthorized();
+        }
+
+        User user = this.userRepository.findById(username).get();
 
         if (!command.oldPassword().equals(user.getPassword())) {
             throw new IncorrectPassword();
@@ -61,27 +67,27 @@ public class AuthCommandHandler {
 
         user.setPassword(command.newPassword());
 
-        this.repository.save(user);
+        this.userRepository.save(user);
     }
 
     public void handle(ChangeUserRoleCommand command) {
         String adminUsername = new StringBuilder(command.token()).reverse().toString();
 
-        if(this.repository.findById(adminUsername).isEmpty()) {
+        if(this.userRepository.findById(adminUsername).isEmpty()) {
             throw new Unauthorized();
         }
 
-        User adminUser = this.repository.findById(adminUsername).get();
+        User adminUser = this.userRepository.findById(adminUsername).get();
 
         if(!adminUser.getRole().equals(Role.ADMIN)){
             throw new Unauthorized();
         }
 
-        if(this.repository.findById(command.username()).isEmpty()) {
+        if(this.userRepository.findById(command.username()).isEmpty()) {
             throw new UserNotFound();
         }
 
-        User user = this.repository.findById(command.username()).get();
+        User user = this.userRepository.findById(command.username()).get();
 
         try {
             user.setRole(Role.valueOf(command.role()));
@@ -89,17 +95,17 @@ public class AuthCommandHandler {
             throw new InvalidRole();
         }
 
-        this.repository.save(user);
+        this.userRepository.save(user);
     }
 
     public void handle(ChangeUserInfoCommand command) {
         String username = new StringBuilder(command.token()).reverse().toString();
 
-        if(this.repository.findById(username).isEmpty()) {
+        if(this.userRepository.findById(username).isEmpty()) {
             throw new Unauthorized();
         }
 
-        User user = this.repository.findById(username).get();
+        User user = this.userRepository.findById(username).get();
 
         user.setFirstName(command.firstName());
         user.setLastName(command.lastName());
@@ -110,22 +116,22 @@ public class AuthCommandHandler {
             throw new InvalidRole();
         }
 
-        this.repository.save(user);
+        this.userRepository.save(user);
     }
 
     public void handle(DeleteUserCommand command) {
         String username = new StringBuilder(command.token()).reverse().toString();
 
-        if (this.repository.findById(username).isEmpty()) {
+        if (this.userRepository.findById(username).isEmpty()) {
             throw new Unauthorized();
         }
 
-        User user = this.repository.findById(username).get();
+        User user = this.userRepository.findById(username).get();
 
         if (!command.password().equals(user.getPassword())) {
             throw new IncorrectPassword();
         }
 
-        this.repository.delete(user);
+        this.userRepository.delete(user);
     }
 }
